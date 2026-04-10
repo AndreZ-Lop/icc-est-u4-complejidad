3abdd80fdfce99d420ed0d581da53c9601984f2e
Andres <y2@Andress-MacBook-Pro.local>
2026-04-10 10:46:07 -0500

    UPD: App file

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        int numero = 10;
+        int [] array = new int []{1,2,3,4,5};
+        System.out.println(numero);
+        for (int i : array){
+            System.out.println(i);
+        }
     }
 }
